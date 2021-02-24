af2937ac7bb40cb2ae4cfd387f5b5158c611d83a
Abdul <abdallahonline22@yahoo.com>
2021-02-24 16:34:39 +0800

    initial commit

---
+++ data.py
@@ -0,0 +1,30 @@
+
+def Articles():
+    articles = [
+            {
+                'id': 1,
+                'title': 'Article One',
+                'body': 'Navbar navigation links build on our .nav options with their own modifier class and require the use of toggler classes for proper responsive styling. Navigation in navbars will also grow to occupy as much horizontal',
+                'author': 'Abdul Yakubu', 
+                'create_date': '2021-02-10'
+
+            },
+            {
+                'id': 2,
+                'title': 'Article Two',
+                'body': 'Navbar navigation links build on our .nav options with their own modifier class and require the use of toggler classes for proper responsive styling. Navigation in navbars will also grow to occupy as much horizontal',
+                'author': 'Abdul Yakubu', 
+                'create_date': '2021-02-10'
+
+            },
+            {
+                'id': 3,
+                'title': 'Article Three',
+                'body': 'Navbar navigation links build on our .nav options with their own modifier class and require the use of toggler classes for proper responsive styling. Navigation in navbars will also grow to occupy as much horizontal',
+                'author': 'Abdul Yakubu', 
+                'create_date': '2021-02-10'
+
+            }
+    ]
+
+    return articles 
